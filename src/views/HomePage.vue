184281d500b70242c212ec7cc671b1bb1ba20587
whiteneofetch <rumordek@gmail.com>
2021-12-25 07:11:21 +0300

    fix

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,25 +1,53 @@
 <template>
-  <div class="home">
-    <div class="product-list">
-      <ProductCard
-        v-for="(product, key) in products"
-        :key="key"
-        :title="product.title"
-        :price="product.price"
-        :imageUrl="product.imageUrl"
-        @add-to-cart="addToCart(product)"
-        :inCart="cartItemsIds.includes(product._id)"
-      />
-    </div>
-  </div>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div class="home">
+        <div class="product-list">
+          <ProductCard
+            v-for="(product, key) in products"
+            :key="key"
+            :title="product.title"
+            :price="product.price"
+            :imageUrl="product.imageUrl"
+            @add-to-cart="addToCart(product)"
+            :inCart="cartItemsIds.includes(product._id)"
+          />
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 import { mapGetters, mapActions, mapMutations } from "vuex";
 import ProductCard from "../components/ProductCard";
 
-export default {
+export default defineComponent({
   components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
     ProductCard,
   },
   mounted() {
@@ -40,10 +68,38 @@ export default {
       addToCart: "addToCart",
     }),
   },
-};
+});
 </script>
 
-<style>
+<style scoped>
+#container {
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
 .product-list {
   display: flex;
 }
